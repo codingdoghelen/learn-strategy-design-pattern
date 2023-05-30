8be0dd1881f91385725e9db41aa8083781f9912d
helen <codinghk2020@gmail.com>
2023-05-30 16:57:36 +0800

    add: WIP leetcodes of 30/5

---
+++ src/main/java/com/demo/strategy/leetcode/ReplaceElementswithGreatestElementonRightSide_1299.java
@@ -0,0 +1,63 @@
+package com.demo.strategy.leetcode;
+
+public class ReplaceElementswithGreatestElementonRightSide_1299 {
+    public static void main(String[] args) {
+        System.out.println(replaceElements(new int[]{17, 18, 5, 4, 6, 1})); // 2
+
+    }
+
+
+    public static int[] replaceElements(int[] arr) {
+        // Two pointers
+
+        if (arr.length <= 1) return new int[]{-1};
+
+
+        int j = 1;
+        int greatest =0;
+        for (int i = 0; i  < arr.length ; i++) {
+
+
+//
+//            while (arr[i] >=arr[j] && j < arr.length-1 ) {
+//                j++;
+//            }
+//
+//            System.out.println(j);
+//
+//            if(i== arr.length-1) {
+//                arr[i] = -1;
+//            }
+//
+//            if (arr[i] < arr[j]){
+//                arr[i] = arr[j];
+//                arr[i+1] = arr[i+2];
+//            }
+
+
+
+//            if(i== arr.length-1) {
+//                arr[i] = -1;
+//            }else if ( arr[j] < arr[i]) {
+////                arr[i] = arr[j];
+//                j+=1;
+//            }  else if ( arr[j] > arr[i]) {
+//                arr[i] = arr[j];
+//                i+=1;
+//            }  else {
+//                arr[i]  = arr[i];
+//            }
+//
+//            if (j < arr.length-1) {
+//
+//                j+=1;
+//            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+
+            System.out.println(arr[i]);
+        }
+        return arr;
+    }
+}
