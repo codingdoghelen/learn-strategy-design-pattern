1f32e9f39492b7ba0b93b38468e65c1540a48bc3
helen <codinghk2020@gmail.com>
2023-07-25 17:23:33 +0800

    add: leetcodes

--- src/main/java/com/demo/strategy/leetcode/TwoSum2InputArrayIsSorted_167.java
+++ src/main/java/com/demo/strategy/leetcode/TwoSum2InputArrayIsSorted_167.java
@@ -1,31 +1,41 @@
 package com.demo.strategy.leetcode;
 
+import java.util.Arrays;
+
 public class TwoSum2InputArrayIsSorted_167 {
 
     public static void main(String[] args) {
         System.out.println(twoSums(new int[]{2, 7, 11, 15}, 9));
+        System.out.println(twoSums(new int[]{2, 3, 4}, 6));
+        System.out.println(twoSums(new int[]{-1, 0}, -1));
+
     }
 
-    public static int twoSums(int[] numbers, int target) {
+    public static int[] twoSums(int[] numbers, int target) {
+
+        // Two pointers
+        // One at start, one at the end
+        // Keep checking if start + end == target
 
         int left = 0;
         int right = numbers.length - 1;
-        int mid = (left + right) / 2;
 
+        int numberLeft = 0;
+        int numberRight = 0;
 
         while (left <= right) {
+            numberLeft = numbers[left];
+            numberRight = numbers[right];
 
-            if (target > numbers[mid]) {
-                left = mid + 1;
-            } else if (target < numbers[mid]) {
-                right = mid - 1;
+            if (numberLeft + numberRight == target) break;
+
+            if ((numberLeft + numberRight) > target) {
+                right--;
             } else {
-                return mid;
-
+                left++;
             }
-
         }
-        return -1;
+        return new int[]{left + 1, right + 1};
     }
 
 
